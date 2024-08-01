e345c95cae26153864249655c594f5f10d008be9
Darrel <Darrelliew6@gmail.com>
2024-08-02 01:17:47 +0800

    last bcrypt commit

--- routes/user.js
+++ routes/user.js
@@ -27,7 +27,7 @@ router.post('/login', async (req, res) => {
         res.status(200).json({ message: 'Login successful', user: user });
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message+`connection secured ` });
     }
 });
 
